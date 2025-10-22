75a016485587fcff70447f1bf180ef88570cff95
23120064 <ngtnhan14@gmail.com>
2025-10-22 10:51:08 +0700

    reset + back

--- js/input.js
+++ js/input.js
@@ -6,12 +6,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
+            if (button.innerText == 'Reset') {
+                inputNumber.innerText = '0';
+                console.log('reset');
+                return;
+            }
+            if (inputNumber.innerText.length >= 19) return;
             let value = (parseInt(button.innerText));
             if (inputNumber.innerText == '0') {
                 inputNumber.innerText = value;
             }
             else {
-                inputNumber.innerText = parseInt(inputNumber.innerText) * 10 + value + "";
+                let rawNumber = inputNumber.innerText.replace(/\./g, '');
+                if (button.innerText == 'Back') {
+                    rawNumber = rawNumber.substring(0, rawNumber.length - 1);
+                }
+                else {
+                    rawNumber = rawNumber + value;
+                }
+                inputNumber.innerText = Number(rawNumber).toLocaleString('en-US').replace(/,/g, '.');
             }
         });
     });
