f421d718e36dec5a1d5482239c6730ee82c54d98
23120064 <ngtnhan14@gmail.com>
2025-10-22 14:36:58 +0700

    testing json + load

--- js/unit-select.js
+++ js/unit-select.js
@@ -1,3 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+
+    async function loadConversionData() {
+        try {
+            const response = await fetch('json/conversions.json');
+
+            if (!response.ok) {
+                throw new Error('Network problem! Status: ' + response.status);
+            }
+
+            const data = await response.json();
+
+            init(data);
+        }
+
+        catch (error) {
+            console.error('Không thể tải dữ liệu:', error);
+        }
+    }
+
+    function init(data) {
+        console.log(data);
+    }
+
+    loadConversionData();
 })
