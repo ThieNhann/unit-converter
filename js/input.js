f7ad25deff8a802d224402c28b4a7905ca692ae1
23120064 <ngtnhan14@gmail.com>
2025-10-22 11:11:41 +0700

    good convert, but decimal places dont work

--- js/input.js
+++ js/input.js
@@ -4,10 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let buttons = document.querySelectorAll('.number-button');
 
+    let convert = 0.3048;
+
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
             if (button.innerText == 'Reset') {
                 inputNumber.innerText = '0';
+                resultNumber.innerText ='0';
                 console.log('reset');
                 return;
             }
@@ -15,6 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
             let value = (parseInt(button.innerText));
             if (inputNumber.innerText == '0') {
                 inputNumber.innerText = value;
+                resultNumber.innerText = (Number(inputNumber.innerText) * convert).toLocaleString('en-US').replace(/,/g, '.'); 
             }
             else {
                 let rawNumber = inputNumber.innerText.replace(/\./g, '');
@@ -24,6 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 else {
                     rawNumber = rawNumber + value;
                 }
+                resultNumber.innerText = (Number(rawNumber) * convert).toLocaleString('en-US').replace(/,/g, '.'); 
                 inputNumber.innerText = Number(rawNumber).toLocaleString('en-US').replace(/,/g, '.');
             }
         });
